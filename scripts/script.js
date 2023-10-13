3813e8f9523403ded80d15148f2003adca177188
shinobirun <adrian.latorre@gmail.com>
2023-10-13 12:27:14 -0300

    cambios en el header del index, en el ifram de contactos, y se trato de agregar una api

--- scripts/script.js
+++ scripts/script.js
@@ -12,4 +12,37 @@ document.addEventListener('DOMContentLoaded', function() {
       avion.style.top = y + 'px';
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navHamburguer = document.querySelector('.nav-hamburguer');
+
+    hamburgerMenu.addEventListener('click', function() {
+        console.log('Click en el menú hamburguesa');
+        navHamburguer.classList.toggle('active');
+        console.log('Clase active toggled');
+    });
+
+    // Para hacer visible el elemento al cargar la página
+    navHamburguer.style.display = 'block'; // o 'flex', o cualquier otro valor que sea apropiado para tu diseño
+});
+
+
+// api dolar hoy
+
+document.addEventListener('DOMContentLoaded', function() {
+  const dolarValueElement = document.getElementById('dolar-value');
+
+  // Realizar la solicitud a la API de Dólar Hoy
+  fetch('https://www.dolarhoy.com/api.php')
+      .then(response => response.json())
+      .then(data => {
+          const dolarValue = data.venta; // Aquí puedes acceder al valor de compra, venta, etc.
+          dolarValueElement.innerText = `El valor del dólar en Argentina es: ${dolarValue}`;
+      })
+      .catch(error => {
+          console.error('Error al obtener el valor del dólar:', error);
+          dolarValueElement.innerText = 'No se pudo obtener el valor del dólar en este momento.';
+      });
+});
   
